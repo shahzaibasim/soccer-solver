6db2b6e660a6fbf85f7684cb63b9a3abdf5fd66a
shahzaibasim <shahzaibasimciit@gmail.com>
2026-07-15 18:18:42 +0500

    adding skeleton for full stack dockerised application

---
+++ backend/app/backend_pre_start.py
@@ -0,0 +1,13 @@
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    logger.info("Boiler plate finished initializing")
+
+
+if __name__ == "__main__":
+    main()
